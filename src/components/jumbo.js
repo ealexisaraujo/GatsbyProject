e8474f1cdc5eface118a6aab84671ab6900b0a17
Alexis Araujo <ala1289@gmail.com>
2019-07-09 17:35:13 -0500

    Consultas en GraphQL desde React

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {StyledJumbo} from '../styles/components'
 
-export default function jumbo() {
+export default function jumbo({description}) {
   return (
     <StyledJumbo>
       <div>
         <h2>¡Consigue el mejor swag exclusivo y especial de Platzi!</h2>
-        <small>Piezas elegantes para los mejores estudiantes.</small>
+        <small>{description}</small>
       </div>
     </StyledJumbo>
   )
