a721ebaf782a6e434441317ed7b2f6ad517c1933
Alexis Araujo <ala1289@gmail.com>
2019-07-17 08:02:05 -0500

    Estilos Globales con styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -3,43 +3,30 @@ import PropTypes from "prop-types"
 import React from "react"
 import {MenuItem, StyledHeader} from '../styles/components'
 
-const Header = ({ siteTitle }) => (
+const Header = ({}) => (
   <StyledHeader>
   <Link to='/'>
     <img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt='logoplatziswag'></img>
   </Link>
-  <nav>
-    <ul>
-      <MenuItem margin>
-        <Link to='/'>
-          Productos
-        </Link>
-      </MenuItem>
-      <MenuItem margin>
-        <a href='http://www.platzi.com'>Platzi</a>
-      </MenuItem>
-      <MenuItem>
-        <Link to='/cart'>
-          <span>
-            <img src="https://i.postimg.cc/L6wpMxLt/cart.png" alt=''></img>
-          </span>
-        </Link>
-      </MenuItem>
-    </ul>
-  </nav>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <nav>
+      <ul>
+        <MenuItem margin>
+          <Link to='/'>
+            Productos
+          </Link>
+        </MenuItem>
+        <MenuItem margin>
+          <a href='http://www.platzi.com'>Platzi</a>
+        </MenuItem>
+        <MenuItem>
+          <Link to='/cart'>
+            <span>
+              <img src="https://i.postimg.cc/L6wpMxLt/cart.png" alt=''></img>
+            </span>
+          </Link>
+        </MenuItem>
+      </ul>
+    </nav>
   </StyledHeader>
 )
 
